e802442d4a69aa2370b8ca70357f9480e279ce89
andrew-vicencio <andrew.v1124@gmail.com>
2022-07-16 15:07:55 -0400

    Added base enemy

---
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BaseEnemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform player;
+    [SerializeField] private Vector3 target;
+
+    [SerializeField] private float maxSpeed = 1f;
+    [SerializeField] private float maxAcceleration = 1f;
+    [SerializeField] private float prefDist = 5f;
+
+    protected void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player").transform;
+        agent.acceleration = maxAcceleration;
+        agent.speed = maxSpeed;
+        agent.autoRepath = true;
+        agent.stoppingDistance = prefDist;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+}
