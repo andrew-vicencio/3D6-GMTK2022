a4e6d37e61f9809256b3308774b3c96944379438
Rafe Osborne <rafe.osborne@gmail.com>
2022-07-17 00:38:58 -0400

    added pause and death menu; needs proper font

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool paused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && PlayerHealth.currentHealth <= 0)
+        {
+            if (paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        paused = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    void Pause()
+    {
+        paused = true;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject deathMenuUI;
 
     public void OnDeath() {
-        Debug.Log("You lose");
+        Time.timeScale = 0f;
+        deathMenuUI.SetActive(true);
+    }
+
+    public static void RestartGame()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("res");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public static void QuitGame()
+    {
+        Debug.Log("q");
+        Application.Quit();
     }
 }
